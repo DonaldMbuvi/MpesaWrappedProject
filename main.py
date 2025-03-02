cc3e96c5c9fcd30c7a83a19a40d18cab8fb64ff5
Newton <123421090+SKNYYYYYYYY@users.noreply.github.com>
2025-03-02 08:34:42 +0300

    added get endpoint to fetch statements from a table and store it in json format for analysis

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
+import inspect
+import json
 from typing import Annotated
-from fastapi import FastAPI, HTTPException, UploadFile, File, Depends
+from fastapi import FastAPI, HTTPException, UploadFile, File, Depends, Query
 import csv
 from pydantic import BaseModel
-import MpesaWrappedProject.demo_models as demo_models
-from MpesaWrappedProject.demo_database import engine, sessionLocal
+from  demo_models import Statement
+from demo_database import engine, sessionLocal
 from sqlalchemy.orm import Session
+from demo_database import Base
 
 # Initialize FastAPI app
 app = FastAPI()
 
 # Create database table if they don't exist
-demo_models.Statement.__table__.create(bind=engine, checkfirst=True)
+Statement.__table__.create(bind=engine, checkfirst=True)
+
 
 # Define Pydantic model for request validation
 class StatementBase(BaseModel):
@@ -32,7 +36,7 @@ db_dependency = Annotated[Session, Depends(get_db)]
 
 # API endpoint to save a statement to the database
 @app.post("/upload-csv/")
-def upload_csv_and_save_to_db(db: db_dependency, file: UploadFile = File(...)):
+def upload_csv_and_save_to_db(db: db_dependency, username: str = Query(...), file: UploadFile = File(...)):
 	#check if file is csv
 	if not file.filename.endswith('.csv'):
 		raise HTTPException(status_code=400, detail="File is not a csv file")
@@ -46,7 +50,27 @@ def upload_csv_and_save_to_db(db: db_dependency, file: UploadFile = File(...)):
 
 	#store csv data in mysql
 	for row in csv_reader:
-		item = demo_models.Statement(date=row[0], name=row[1], amt_in=int(row[2]), amt_out=int(row[3]))
+		item = Statement(username = username, date=row[0], name=row[1], amt_in=int(row[2]), amt_out=int(row[3]))
 		db.add(item)
 	db.commit()
 	db.close()
+
+# API endpoint for getting the saved transactions
+@app.get('/get_transactions/')
+def get_transactions(db: db_dependency,  username: str = Query(...)):
+    user_statement = db.query(Statement).filter(Statement.username == username).all()
+	# Convert to dictionary format
+    user_statement_dict = [
+		{
+			"id": row.id,
+			"name": row.name,
+			"amt_out": row.amt_out,
+			"amt_in": row.amt_in,
+			"date": str(row.date) 
+		}
+		for row in user_statement
+	]
+	#save the statement in json file
+    with open("user_statement.json", "w") as file:
+        json.dump(user_statement_dict, file, indent=4)
+    return "transaction saved in json format"

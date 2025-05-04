4c0ae44a4bacf81f90c93f49aef188bf32e01e10
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2025-05-04 19:47:43 +0300

    externalize html2canvas to resolve import issue

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-  ],
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['html2canvas'],
+    },
+  },
   server: {
     host: true, // Allow LAN and local access
     port: 5173, // Force port 5173
-  }
-})
+  },
+});
